354f9d3fb53815638884e9f998604347091268de
Anna Zattoni <63807607+annazattoni@users.noreply.github.com>
2020-08-21 18:24:27 +0200

    Update portfolio.jsx
    fix

--- src/layouts/article/portfolio.jsx
+++ src/layouts/article/portfolio.jsx
@@ -46,7 +46,7 @@ export default class PortfolioTemplate extends React.Component {
                       target="_system"
                     >
                       <i className="icon icon-annazattoni-arrow-right"></i>{" "}
-                      Vedi il lavoro
+                      Guarda ciò che ho fatto!
                     </a>
                   )}
                 </div>
